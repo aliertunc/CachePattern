29809ef939e82891c9f7e8ab0e360aecca2c3103
Ali Ertunç <ertuncali07@gmail.com>
2022-12-17 22:54:55 +0300

    First Commit

---
+++ CacheManager/CacheManager.cs
@@ -0,0 +1,67 @@
+﻿using System; 
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory; 
+
+namespace CacheManager
+{
+    public class CacheManager
+    {
+        private readonly IMemoryCache _cache;
+
+        public CacheManager(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public void AddCache(string key, object value, double seconds)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetAbsoluteExpiration(TimeSpan.FromSeconds(seconds));
+            _cache.Set(key, value, cacheEntryOptions);
+        }
+        public async Task AddCacheAsync(string key, object value, double seconds)
+        {
+            await Task.Run(() =>
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetAbsoluteExpiration(TimeSpan.FromSeconds(seconds));
+                _cache.Set(key, value, cacheEntryOptions);
+            });
+        }
+        public void RemoveCache(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public object GetCache(string key)
+        {
+            object value;
+            if (_cache.TryGetValue(key, out value))
+            {
+                return value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<object> GetCacheAsync(string key)
+        {
+            object value = null;
+            await Task.Run(() =>
+            {
+                if (_cache.TryGetValue(key, out value))
+                {
+                    return value;
+                }
+                else
+                { 
+                    return value;
+                }
+            });
+
+            return value;
+        }
+
+    }
+}
---
+++ CachePattern.Unit.Test/CacheManagerTests.cs
@@ -0,0 +1,90 @@
+﻿ 
+using Microsoft.Extensions.Caching.Memory; 
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CachePattern.Unit.Test
+{
+    public class CacheManagerTests
+    {
+        private readonly IMemoryCache _cache;
+        public CacheManagerTests()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+        }
+        [Fact]
+        public void AddCacheTest()
+        {
+            CacheManager.CacheManager manager = new CacheManager.CacheManager(_cache);
+            string keyName = "theKeyName";
+            string keyValue = "theKeyValue";
+
+            manager.AddCache(keyName, keyValue, 5);
+            var cacheValue = manager.GetCache(keyName);
+
+            Assert.NotNull(cacheValue);
+            Assert.Equal(keyValue, cacheValue);
+        }
+        [Fact]
+        public async Task AddCacheTestAsync()
+        {
+            CacheManager.CacheManager manager = new CacheManager.CacheManager(_cache);
+            string keyName = "theKeyName";
+            string keyValue = "theKeyValue";
+
+            await manager.AddCacheAsync(keyName, keyValue, 5);
+            var cacheValue = manager.GetCache(keyName);
+
+            Assert.NotNull(cacheValue);
+            Assert.Equal(keyValue, cacheValue);
+        }
+
+        [Fact]
+        public void RemoveCacheTest()
+        {
+            CacheManager.CacheManager manager = new CacheManager.CacheManager(_cache);
+            string keyName = "theKeyName";
+            string keyValue = "theKeyValue";
+
+            manager.AddCache(keyName, keyValue, 5);
+            var cacheValue = manager.GetCache(keyName);
+            manager.RemoveCache(keyName);
+            var removedCacheValue = manager.GetCache(keyName);
+
+            Assert.NotNull(cacheValue);
+            Assert.Null(removedCacheValue);
+            Assert.Equal(keyValue, cacheValue);
+        }
+        [Fact]
+        public async Task GetCacheAsyncTest()
+        {
+            CacheManager.CacheManager manager = new CacheManager.CacheManager(_cache);
+            string keyName = "theKeyName";
+            string keyValue = "theKeyValue";
+
+            manager.AddCache(keyName, keyValue, 5);
+           var cacheValue=  await manager.GetCacheAsync(keyName);
+
+            Assert.NotNull(cacheValue);
+            Assert.Equal(keyValue, cacheValue);
+        }
+
+        [Fact]
+        public void GetTestAfterExpire()
+        {
+            CacheManager.CacheManager manager = new CacheManager.CacheManager(_cache);
+            string keyName = "theKeyName";
+            string keyValue = "theKeyValue";
+
+            manager.AddCache(keyName, keyValue, 5);
+            var cacheValue = manager.GetCache(keyName);
+            Thread.Sleep(5000); 
+            var cacheValueExpired = manager.GetCache(keyName);
+
+            Assert.Null(cacheValueExpired);
+            Assert.NotNull(cacheValue);
+            Assert.Equal(keyValue, cacheValue);
+        }
+    }
+}
